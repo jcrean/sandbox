357e71feef9602b5cd46e8e12f7b52e3bc9981c4
Joshua Crean <joshua.crean@gmail.com>
2011-07-23 13:06:33 -0400

    set up basic jetty server

--- clj-web-ldap/project.clj
+++ clj-web-ldap/project.clj
@@ -2,5 +2,9 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.2.0"]
          	 [org.clojure/clojure-contrib "1.2.0"]
-                 [clj-ldap "0.0.4"]]
-  :dev-dependencies [[swank-clojure "1.2.0"]])
+                 [clj-ldap "0.0.4"]
+                 [compojure/compojure "0.6.5"]]
+  :dev-dependencies [[swank-clojure "1.2.0"]
+                     [lein-ring  "0.4.5"]
+                     [ring/ring-jetty-adapter "0.3.11"]]
+  :ring {:handler clj-web-ldap.core/app})
--- clj-web-ldap/src/clj_web_ldap/core.clj
+++ clj-web-ldap/src/clj_web_ldap/core.clj
@@ -1,6 +1,12 @@
 (ns clj-web-ldap.core
   (:require
-   [clj-ldap.client     :as ldap]))
+   [compojure.route     :as route]
+   [compojure.handler   :as handler]
+   [clj-ldap.client     :as ldap])
+  (:use
+   [compojure.core]
+   [ring.adapter.jetty  :only [run-jetty]]))
+
 
 
 (def ldap-server (ldap/connect {:host "ec2-50-19-176-178.compute-1.amazonaws.com"
@@ -8,9 +14,35 @@
                                 :bind-dn "uid=jcrean,ou=users,dc=relayzone,dc=com"
                                 :password "jcjcjc"}))
 
+(defroutes main-routes
+  (GET "/" [] "<h1>Hello World Wide Web!</h1>")
+  (route/resources "/")
+  (route/not-found "Page not found"))
+
+
+(defn app []
+  (handler/site main-routes))
+
+(defonce *server* (atom nil))
+
+(defn start-server []
+  (when (nil? @*server*)
+    (reset! *server* (run-jetty (app) {:port 8080 :join? false}))))
+
+(defn stop-server []
+  (when-not (nil? @*server*)
+    (.stop @*server*)
+    (reset! *server* nil)))
 
 
 
-(ldap/bind ldap-server "uid=jcrean,ou=users,dc=relayzone,dc=com" "jcjcjc")
 
-(ldap/get ldap-server "uid=jcrean,ou=users,dc=relayzone,dc=com")
+(comment
+
+  (start-server)
+
+  (stop-server)
+
+  (ldap/bind ldap-server "uid=jcrean,ou=users,dc=relayzone,dc=com" "jcjcjc")
+
+  (ldap/get ldap-server "uid=jcrean,ou=users,dc=relayzone,dc=com"))
